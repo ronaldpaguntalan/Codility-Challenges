b54396303a7cfb8897e6b207116d8ae4aa7d18f8
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2024-05-05 01:28:35 +0800

    added an argument for frogriverone

--- Lesson 4 - Counting Elements/FrogRiverOne.py
+++ Lesson 4 - Counting Elements/FrogRiverOne.py
@@ -1,9 +1,13 @@
 def solution(X, A):
     position_covered = set()
     
+    
     for i in range(len(A)):
         position_covered.add(A[i])
+        print(position_covered)
         if len(position_covered) == X:
             return i
     return -1
-    
+
+earliest_time = solution(5, [1, 3, 1, 4, 2, 3, 5, 4])
+print(earliest_time)
